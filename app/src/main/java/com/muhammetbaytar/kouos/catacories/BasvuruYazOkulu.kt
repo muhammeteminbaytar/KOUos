70d9c4daeb5dc638c78177632f7a3c8e77346f1a
Muhammmet Emin BAYTAR <muhammet2000byt@gmail.com>
2021-11-01 21:16:43 +0300

    Begin Yaz OKulu Screen design

--- app/src/main/java/com/muhammetbaytar/kouos/catacories/BasvuruYazOkulu.kt
+++ app/src/main/java/com/muhammetbaytar/kouos/catacories/BasvuruYazOkulu.kt
@@ -2,6 +2,10 @@ package com.muhammetbaytar.kouos.catacories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import com.muhammetbaytar.kouos.R
 
 class BasvuruYazOkulu : AppCompatActivity() {
@@ -9,6 +13,24 @@ class BasvuruYazOkulu : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basvuru__yaz_okulu)
 
-        this.title="Yaz Okulu Başvuru"
+        this.title = "Yaz Okulu Başvuru"
+        getFireStoreData()
+    }
+
+    fun getFireStoreData() {
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("Users").whereEqualTo("userId",auth.uid).addSnapshotListener { value, error ->
+            if (error != null) {
+                Toast.makeText(this, "Something went wrong !", Toast.LENGTH_LONG).show()
+            } else {
+                if (value != null) {
+                    for (document in value.documents){
+                        println(document.get("userAdSoyad").toString())
+                    }
+                }
+            }
+        }
     }
 }
